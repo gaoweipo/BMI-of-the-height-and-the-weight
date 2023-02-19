aead6131dace061ccad52671cd32c360bf04a37b
gaoweipo <gaoweipo@hotmail.com>
2023-02-19 13:46:27 -0500

    Java exemple 01: Créez la classe BMlexponent, déclarez la variable height(double) pour enregistrer la taille en mètres, déclarez la variable weight(int) pour enregistrer le poids en kilogrammes, et calculez l'indice BMI selon la formule BMI=weight/(height X height).

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double height, weight, BMI;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your height in m here:");
+        height = sc.nextDouble();
+        System.out.println("Enter your weight in Kg here:");
+        weight = sc.nextDouble();
+        BMI = weight / (height * height);
+        if (BMI < 18.5) {
+            System.out.println("Underweight.");
+        }
+        if (BMI >= 18.5 && BMI < 24.9) {
+            System.out.println("Normal.");
+        }
+        if (BMI >= 24.9 && BMI < 29.9) {
+            System.out.println("Overweight.");
+        }
+        if (BMI >= 29.9) {
+            System.out.println("Obesity.");
+        }
+
     }
 }
